b948791b11e5bdfc53091c5a5e622bfb6fe33530
Samuel Dobson <dobsonsa68@gmail.com>
2021-10-08 17:34:26 +1300

    Clean up old code a bit

--- myImageCode.py
+++ myImageCode.py
@@ -1,37 +1,18 @@
 from PIL import Image, ImageDraw, ImageFont
 import sys
+import argparse
+
 
 # Get our parameters from the command line
-if len(sys.argv) == 6:
-	image_file_path = sys.argv[1]
-	out_file_path = sys.argv[2]
-	repeat_text = sys.argv[3]
-	scale = float(sys.argv[4])
-	alpha = int(sys.argv[5])
-elif len(sys.argv) == 5:
-	image_file_path = sys.argv[1]
-	out_file_path = sys.argv[2]
-	repeat_text = sys.argv[3]
-	scale = float(sys.argv[4])
-	alpha = 0
-elif len(sys.argv) == 4:
-	image_file_path = sys.argv[1]
-	out_file_path = sys.argv[2]
-	repeat_text = sys.argv[3]
-	scale = 1
-	alpha = 0
-elif len(sys.argv) == 3:
-	image_file_path = sys.argv[1]
-	out_file_path = sys.argv[2]
-	repeat_text = "MESH"
-	scale = 1
-	alpha = 0
-else:
-	print("USAGE: <IMAGE PATH> <OUTFILE> [TEXT] [SCALE FACTOR] [USE TRANSPARENT]")
-	sys.exit(0)
+parser = argparse.ArgumentParser(description='Turns an image into coloured BB-Code.')
+parser.add_argument("image", help="path to image file")
+parser.add_argument("outfile", help="path to output file")
+parser.add_argument("--text", help="text to repeat", default="MESH")
+parser.add_argument("--scale", help="factor to scale image by", default=1.0)
+parser.add_argument("--alpha", help="whether to use transparency", action="store_true")
+args = parser.parse_args()
 
-repeat_text_array = list(repeat_text)
-	
+
 def scale_image(image, new_width=100, new_height=0):
     (original_width, original_height) = image.size
     
@@ -45,81 +26,71 @@ def scale_image(image, new_width=100, new_height=0):
 # Open the image
 im = None
 try:
-    im = Image.open(image_file_path).convert('RGBA')
+    im = Image.open(args.image).convert('RGBA')
 except Exception as e:
-    print("Unable to open image file {image_filepath}.".format(image_filepath=image_file_path))
+    print("Unable to open image file {}.".format(args.image))
     print(e)
     sys.exit(1)
 
 # Get the amount of text required to cover the image
 drawer = ImageDraw.Draw(im)
 font = ImageFont.truetype('Verdana.ttf', 14)
-fontSize = drawer.textsize(repeat_text, font=font)
-fontOffset = font.getoffset(repeat_text)
+fontSize = drawer.textsize(args.text, font=font)
+fontOffset = font.getoffset(args.text)
 
-charsAcross = int((im.size[0]/(fontSize[0]+fontOffset[0]))*len(repeat_text)*scale)
-charsHigh = int((im.size[1]/(fontSize[1]+fontOffset[1]))*scale)
+charsAcross = int((im.size[0]/(fontSize[0]+fontOffset[0]))*len(args.text)*float(args.scale))
+charsHigh = int((im.size[1]/(fontSize[1]+fontOffset[1]))*float(args.scale))
 
 # Scale the image to one pixel per character
 image = scale_image(im, charsAcross, charsHigh)
-pixels_in_image = list(image.getdata())
-count = 0
 
-output = ""
+char_count = 0
+current_line = ""
 image_ascii = []
 previous = (0, 0, 0, 1)
-color = "#000000"
-transparent = False
 
 # Iterate over the pixels in the image
-for pixel_value in pixels_in_image:
+for pixel_value in image.getdata():
     r,g,b,a = pixel_value
-    bbcode = ""
-    
-    if count % charsAcross == 0: # start of the line
+
+    if char_count % charsAcross == 0: # start of the line
         previous = ((r * a), (g * a), (b * a), a)
         begin = True
-        end = False
     elif (abs(previous[0] - (r * a)) < 5) and (abs(previous[1] - (g * a)) < 5) and (abs(previous[2] - (b * a)) < 5) and (abs(previous[3] - a) < 5): # somewhere in the middle, check difference with previous char
-        end = False # no difference so don't end or start again
+        # no difference so don't end or start again
         begin = False
     else: # too large difference, we end and begin a new one
         previous = ((r * a), (g * a), (b * a), a)
-        end = True
+        current_line += "[/color]"
         begin = True
 	
 	# Get the individual colour components as hexadecimal strings
-    a = a/255
+    a = a/float(255)
     r = hex(int(r * a)).split('x')[1].zfill(2)
     g = hex(int(g * a)).split('x')[1].zfill(2)
     b = hex(int(b * a)).split('x')[1].zfill(2)
 
     color = "#" + str(r) + str(g) + str(b)
-	
-	# If we have set the USE ALPHA parameter and the alpha is less than 10%
-    if a < 0.1 and alpha == 1:
+
+    # If we have set the --alpha parameter and the alpha is less than 10%
+    if a < 0.1 and args.alpha:
         color = "transparent"
-		
+
 	# Choose the character we need from the string
-    character = repeat_text_array[count % len(repeat_text)]
-    
-    if end:
-        bbcode += "[/color]"
+    character = args.text[char_count % len(args.text)]
+
     if begin:
-        bbcode += "[color=" + color + "]"
-        
-    bbcode += character 
+        current_line += "[color=" + color + "]"
 
-    output += bbcode
-    
-    if count % charsAcross == (charsAcross-1):
-        output += "[/color]"
-        image_ascii.append(output)
-        output = ""
+    current_line += character 
 
-    count += 1
+    if char_count % charsAcross == (charsAcross-1):
+        current_line += "[/color]"
+        image_ascii.append(current_line)
+        current_line = ""
+    char_count += 1
 
 # Write the output to the file
-f = open(out_file_path, 'w')
+f = open(args.outfile, 'w')
 f.write("\n".join(image_ascii))
 f.close()
